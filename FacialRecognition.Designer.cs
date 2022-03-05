31e344241b2306723d141c837123a5ba2699af0e
TechCorp07 <poso.draxy@gmail.com>
2022-03-05 19:08:18 +0200

    fixed merge conflicts

--- FacialRecognition.Designer.cs
+++ FacialRecognition.Designer.cs
@@ -28,33 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD:Form2.Designer.cs
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
-=======
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FacialRecognition));
-<<<<<<< HEAD
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
->>>>>>> dcc95d296dd0f8355c960e381c46ff820514abe4:FacialRecognition.Designer.cs
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-=======
+
             this.addUser_picBox = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -85,7 +60,6 @@
             this.addUser_picBox.TabStop = false;
             this.addUser_picBox.Click += new System.EventHandler(this.addUser_picBox_Click);
             // 
->>>>>>> ffed482e01892e72b3404ccda08ae50f9ff9fc02
             // label1
             // 
             this.label1.AutoSize = true;
@@ -108,8 +82,6 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Add Student";
             // 
-<<<<<<< HEAD
-=======
             // addStudent_picBox
             // 
             this.addStudent_picBox.Image = global::Facial_Recognition.Properties.Resources.stud_add1;
@@ -120,7 +92,6 @@
             this.addStudent_picBox.TabIndex = 2;
             this.addStudent_picBox.TabStop = false;
             // 
->>>>>>> ffed482e01892e72b3404ccda08ae50f9ff9fc02
             // label3
             // 
             this.label3.AutoSize = true;
@@ -132,8 +103,6 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Attendance";
             // 
-<<<<<<< HEAD
-=======
             // checkattendance_picBox
             // 
             this.checkattendance_picBox.Image = global::Facial_Recognition.Properties.Resources.attend;
@@ -144,7 +113,6 @@
             this.checkattendance_picBox.TabIndex = 4;
             this.checkattendance_picBox.TabStop = false;
             // 
->>>>>>> ffed482e01892e72b3404ccda08ae50f9ff9fc02
             // label4
             // 
             this.label4.AutoSize = true;
@@ -156,8 +124,6 @@
             this.label4.TabIndex = 7;
             this.label4.Text = "Register";
             // 
-<<<<<<< HEAD
-=======
             // register_picBox
             // 
             this.register_picBox.Image = global::Facial_Recognition.Properties.Resources.db;
@@ -167,7 +133,6 @@
             this.register_picBox.TabIndex = 6;
             this.register_picBox.TabStop = false;
             // 
->>>>>>> ffed482e01892e72b3404ccda08ae50f9ff9fc02
             // label5
             // 
             this.label5.AutoSize = true;
@@ -207,51 +172,6 @@
             this.logo_picBox.TabIndex = 12;
             this.logo_picBox.TabStop = false;
             // 
-<<<<<<< HEAD
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::Facial_Recognition.Properties.Resources.db;
-            this.pictureBox4.Location = new System.Drawing.Point(498, 12);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(101, 75);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 6;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::Facial_Recognition.Properties.Resources.attend;
-            this.pictureBox3.Location = new System.Drawing.Point(331, 12);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(101, 75);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 4;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Facial_Recognition.Properties.Resources.stud_add1;
-            this.pictureBox2.Location = new System.Drawing.Point(161, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(101, 75);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Facial_Recognition.Properties.Resources.user_add;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(101, 75);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // Form2
-=======
-            // FacialRecognition
->>>>>>> ffed482e01892e72b3404ccda08ae50f9ff9fc02
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -271,19 +191,12 @@
             this.Controls.Add(this.addUser_picBox);
             this.Name = "FacialRecognition";
             this.Text = "Facial Recognition Attendance Sytem";
-<<<<<<< HEAD
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-=======
+
             ((System.ComponentModel.ISupportInitialize)(this.addUser_picBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.addStudent_picBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.checkattendance_picBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.register_picBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.logo_picBox)).EndInit();
->>>>>>> ffed482e01892e72b3404ccda08ae50f9ff9fc02
             this.ResumeLayout(false);
             this.PerformLayout();
 
